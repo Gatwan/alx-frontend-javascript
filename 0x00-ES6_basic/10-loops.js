90b19758834e7d7b3933a438beed85060c05fde1
Gatwan <abigailgathigia@gmail.com>
2023-07-04 11:18:55 +0300

    fix styling error

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const idx of array) {
-    const value = idx;    
+    const value = idx;
     newArray.push(appendString + value);
   }
 
