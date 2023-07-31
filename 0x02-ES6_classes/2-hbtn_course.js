da466f3bc5ec23c52a6c3bea72316285e8b66abe
Gatwan <abigailgathigia@gmail.com>
2023-07-31 09:32:49 +0300

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -20,6 +20,9 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
     this._name = name;
   }
 
@@ -28,6 +31,9 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
     this._length = length;
   }
 
@@ -36,6 +42,9 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array');
+    }
     this._students = students;
   }
 }
